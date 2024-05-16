816bf7d4fef0134b576d8376c11299c8d4812a46
Ali Haider <ali1500haider@gmail.com>
2024-05-16 17:38:46 +0300

    added joi validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,32 @@
+const joi = require("joi");
+
+const createUserSchema = joi.object().keys({
+  username: joi.string().required(),
+  age: joi.string().required(),
+  password: joi.string().min(3).max(20).required(),
+});
+
 module.exports = {
-  getUsers: (req, res) => {
-    res.send({
-      message: "Gets all users",
-    });
-  },
   createUser: (req, res) => {
+    console.log(req.body);
     res.send({
       message: "Create a user",
     });
+  },
+  getUsers: async (req, res) => {
+    try {
+      console.log("control before validate");
+      console.log(req.body);
+      const validate = await createUserSchema.validateAsync(req.body);
+      console.log("control after validate");
+      console.log(validate, "validate");
+      res.send({
+        message: validate,
+      });
+    } catch (error) {
+      res.send({
+        message: error.message,
+      });
+    }
   },
 };

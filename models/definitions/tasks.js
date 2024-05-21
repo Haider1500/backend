6e4cf53292977e6c794623bddbb56f2e823aa465
Ali Haider <ali1500haider@gmail.com>
2024-05-21 14:32:26 +0500

    add models

---
+++ models/definitions/tasks.js
@@ -0,0 +1,16 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../../bin/dbConnection");
+
+class tasks extends Model {}
+
+tasks.init(
+  {
+    taskId: {
+      primaryKey: true,
+      type: DataTypes.STRING(255),
+    },
+  },
+  { timestamps: true, paranoid: true, tableName: "tasks", sequelize }
+);
+
+module.exports = tasks;
---
+++ models/index.js
@@ -0,0 +1,14 @@
+const sequelize = require("../bin/dbConnection");
+
+const users = require("./definitions/users");
+const tasks = require("./definitions/tasks");
+const taskHasType = require("./definitions/taskHasType");
+
+const models = { users, tasks, taskHasType };
+
+const db = {};
+db.sequelize = sequelize;
+
+sequelize.models = models;
+
+module.exports = { db, models };

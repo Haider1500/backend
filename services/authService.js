3eadcc49b1f2782da1caf5522672d5f41b9c35d7
Ali Haider <ali1500haider@gmail.com>
2024-06-05 20:19:42 +0300

    add logout in service

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,18 @@
 module.exports = {
   login: async () => {
     try {
+      return {
+        message: "user has logged in",
+      };
+    } catch (error) {
+      return { error: error };
+    }
+  },
+  logout: async () => {
+    try {
+      return {
+        message: "user has logged out",
+      };
     } catch (error) {
       return { error: error };
     }
